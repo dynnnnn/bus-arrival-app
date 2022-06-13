61591362df60ac4f30028116bb0ab42878e9e28d
gladyn <gladynwong@gmail.com>
2022-06-13 11:10:56 +0800

    added loadbus function

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+
+const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
 export default function App() {
 
   const [loading, setLoading] = useState(true);
 
+  function loadBus() {
+    fetch(BUSSTOP_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log(responseData);
+  });
+}
+
+useEffect(() => {
+  loadBus();
+}, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bus Arrival Time: </Text>
-      <Text style={styles.arrivalTime}> {loading ? <ActivityIndicator color={'pink'}/> : "Loaded"} </Text>
-      <TouchableOpacity style={styles.button} onPress={() => setLoading(true)}><Text>Refresh</Text></TouchableOpacity>
+      <Text style={styles.arrivalTime}> {loading ? <ActivityIndicator color={'grey'}/> : "Loaded"} </Text>
+      <TouchableOpacity style={styles.button} onPress={() => setLoading(true)}><Text style={styles.buttonText}>Refresh</Text></TouchableOpacity>
     </View>
   );
 }
@@ -28,11 +44,14 @@ const styles = StyleSheet.create({
     padding: 20,
     marginVertical: 20,
   },
+  buttonText: {
+    fontSize: 20, 
+  },
   title: {
     fontSize: 30,
   },
   arrivalTime: {
-    fontSize: 30,
+    fontSize: 40,
     padding: 20,
   },
   
