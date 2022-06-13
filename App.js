1e21a3ec9b036b91c5c7931ee2387563efeff5a6
gladyn <gladynwong@gmail.com>
2022-06-13 10:55:28 +0800

    add title

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
+import React, { useState } from 'react';
+
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus Arrival Time: </Text>
+      <Text style={styles.arrivalTime}> {loading ? <ActivityIndicator color={'pink'}/> : "Loaded"} </Text>
+      <TouchableOpacity style={styles.button} onPress={() => setLoading(true)}><Text>Refresh</Text></TouchableOpacity>
     </View>
   );
 }
@@ -17,4 +23,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'lightgrey',
+    padding: 20,
+    marginVertical: 20,
+  },
+  title: {
+    fontSize: 30,
+  },
+  arrivalTime: {
+    fontSize: 30,
+    padding: 20,
+  },
+  
 });
