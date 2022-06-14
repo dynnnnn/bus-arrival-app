40aea2fefda28c906bc7cd8c0b438aae7fc24db4
gladyn <gladynwong@gmail.com>
2022-06-14 10:31:10 +0800

    next bus timing added

--- App.js
+++ App.js
@@ -8,9 +8,12 @@ export default function App() {
 
   const [loading, setLoading] = useState(true);
   const [arrival, setArrival] = useState("");
+  const [arrival2, setArrival2] = useState("");
+ const [bus, setBus] = useState("");
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
 
   function loadBus() {
+    setLoading(true);
     fetch(BUSSTOP_URL)
     .then((response) => {
       return response.json();
@@ -21,20 +24,38 @@ export default function App() {
         (service) => service.no === "155"
         )[0];
       console.log("My bus: " + myBus);
-      console.log(myBus.next.time);
-      setArrival(myBus.next.time);
+      console.log(myBus.no);
+      console.log(myBus.next.duration_ms);
+      console.log(myBus.subsequent.duration_ms);
+      var mins = Math.floor(myBus.next.duration_ms / 60000);
+      var sec = ((myBus.next.duration_ms % 60000) / 1000).toFixed(0);
+      var mins2 = Math.floor(myBus.subsequent.duration_ms / 60000);
+      var sec2 = ((myBus.subsequent.duration_ms % 60000) / 1000).toFixed(0);
+      setArrival(mins + " minutes " + sec + " seconds");
+      setArrival2(mins2 + " minutes " + sec2 + " seconds");
+      setBus(myBus.no);
       setLoading(false);
     }); 
 }
 
+
+
 useEffect(() => {
   loadBus();
-}, []);
+},[]);
+
+useEffect(() => {
+  const interval = setInterval(loadBus, 5000);
+  return () => clearInterval(interval);
+},[]);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bus Arrival Time: </Text>
+    <Text style={styles.title}>Bus Number: { bus } </Text>
+      <Text style={styles.title}>Bus Arriving In: </Text>
       <Text style={styles.arrivalTime}> {loading ? <ActivityIndicator color={'grey'}/> : arrival} </Text>
+      <Text style={styles.title}>Next Bus Arriving In: </Text>
+      <Text style={styles.arrivalTime}> {loading ? <ActivityIndicator color={'grey'}/> : arrival2} </Text>
       <TouchableOpacity style={styles.button} onPress={() => setLoading(true)}><Text style={styles.buttonText}>Refresh</Text></TouchableOpacity>
     </View>
   );
@@ -57,9 +78,10 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 30,
+    padding: 20,
   },
   arrivalTime: {
-    fontSize: 40,
+    fontSize: 20,
     padding: 20,
   },
   
